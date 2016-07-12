cf912a8bc5ce2db4db9acbdd725c9c019d758747
Melanie White <melwhite.a@gmail.com>
2016-07-12 13:31:01 -0400

    edit id.

--- js/progressbar.js
+++ js/progressbar.js
@@ -1,11 +1,11 @@
 // progressbar.js@1.0.0 version is used
 // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
 
-var bar = new ProgressBar.Line(opening_line, {
+var bar = new ProgressBar.Line(openingline, {
   strokeWidth: 4,
   easing: 'easeInOut',
   duration: 1400,
-  color: '#FFEA82',
+  color: '#ff7800',
   trailColor: '#eee',
   trailWidth: 1,
   svgStyle: {width: '100%', height: '100%'}
