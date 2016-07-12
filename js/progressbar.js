1eac94f1f5758c0604aaa4fb4bd92f25a6836103
Melanie White <melwhite.a@gmail.com>
2016-07-12 13:39:20 -0400

    add quotes around id.

--- js/progressbar.js
+++ js/progressbar.js
@@ -1,14 +1,17 @@
 // progressbar.js@1.0.0 version is used
 // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
 
-var bar = new ProgressBar.Line(openingline, {
-  strokeWidth: 4,
-  easing: 'easeInOut',
-  duration: 1400,
-  color: '#ff7800',
-  trailColor: '#eee',
-  trailWidth: 1,
-  svgStyle: {width: '100%', height: '100%'}
-});
+window.onload = function onLoad() {
 
-bar.animate(1.0);  // Number from 0.0 to 1.0
+	var bar = new ProgressBar.Line('#openingline', {
+	  strokeWidth: 4,
+	  easing: 'easeInOut',
+	  duration: 1400,
+	  color: '#ff7800',
+	  trailColor: '#eee',
+	  trailWidth: 1,
+	  svgStyle: {width: '100%', height: '100%'}
+	});
+
+	bar.animate(1.0);  // Number from 0.0 to 1.0
+}
