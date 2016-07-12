04e45efb3f369fbc0ffd1f5be7236f25c6a2d0b9
Melanie White <melwhite.a@gmail.com>
2016-07-11 23:06:31 -0400

    add progressbar.js. Fix nav a.

---
+++ js/progressbar.js
@@ -0,0 +1,14 @@
+// progressbar.js@1.0.0 version is used
+// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+
+var bar = new ProgressBar.Line(opening_site, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#FFEA82',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+
+bar.animate(1.0);  // Number from 0.0 to 1.0
