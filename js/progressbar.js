90ddf7dccede0d3c4fcfcbd2653a731dec60da00
Melanie White <melwhite.a@gmail.com>
2016-07-12 22:37:24 -0400

    comment out code

--- js/progressbar.js
+++ js/progressbar.js
@@ -44,8 +44,8 @@ window.onload = function onLoad() {
 	  }
 	});
 
-	bar.text.style.fontFamily = 'Helvetica, sans-serif';
-	bar.text.style.fontSize = '2em';
+	//bar.text.style.fontFamily = 'Helvetica, sans-serif';
+	//bar.text.style.fontSize = '2em';
 
 	bar.animate(1.0);  // Number from 0.0 to 1.0
 }
