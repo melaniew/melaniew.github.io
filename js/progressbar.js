a3023973dec2576c44c660d756dfd9e247919079
Melanie White <melwhite.a@gmail.com>
2016-07-14 18:47:25 -0400

    add more ids

--- js/progressbar.js
+++ js/progressbar.js
@@ -15,7 +15,7 @@ window.onload = function onLoad() {
 
 	bar.animate(1.0);  // Number from 0.0 to 1.0
 
-	var bar = new ProgressBar.Circle('#css #uxd', {
+	var bar = new ProgressBar.Circle('#css', {
 
 	  color: '#aaa',
 	  // This has to be the same size as the maximum width to
@@ -48,4 +48,91 @@ window.onload = function onLoad() {
 	//bar.text.style.fontSize = '2em';
 
 	bar.animate(1.0);  // Number from 0.0 to 1.0
+	
+	var bar = new ProgressBar.Circle('#jscript', {
+
+	  color: '#aaa',
+	  // This has to be the same size as the maximum width to
+	  // prevent clipping
+	  strokeWidth: 4,
+	  trailWidth: 1,
+	  easing: 'easeInOut',
+	  duration: 2200,
+	  text: {
+	    autoStyleContainer: false
+	  },
+	  from: { color: '#aaa', width: 1 },
+	  to: { color: '#ff7800', width: 4 },
+	  // Set default step function for all animate calls
+	  step: function(state, circle) {
+	    circle.path.setAttribute('stroke', state.color);
+	    circle.path.setAttribute('stroke-width', state.width);
+
+	    var value = Math.round(circle.value() * 100);
+	    if (value === 0) {
+	      circle.setText('');
+	    } else {
+	      circle.setText(value);
+	    }
+
+	  }
+	});
+	
+	var bar = new ProgressBar.Circle('#ruby', {
+
+	  color: '#aaa',
+	  // This has to be the same size as the maximum width to
+	  // prevent clipping
+	  strokeWidth: 4,
+	  trailWidth: 1,
+	  easing: 'easeInOut',
+	  duration: 2200,
+	  text: {
+	    autoStyleContainer: false
+	  },
+	  from: { color: '#aaa', width: 1 },
+	  to: { color: '#ff7800', width: 4 },
+	  // Set default step function for all animate calls
+	  step: function(state, circle) {
+	    circle.path.setAttribute('stroke', state.color);
+	    circle.path.setAttribute('stroke-width', state.width);
+
+	    var value = Math.round(circle.value() * 100);
+	    if (value === 0) {
+	      circle.setText('');
+	    } else {
+	      circle.setText(value);
+	    }
+
+	  }
+	});
+	
+	var bar = new ProgressBar.Circle('#uxd', {
+
+	  color: '#aaa',
+	  // This has to be the same size as the maximum width to
+	  // prevent clipping
+	  strokeWidth: 4,
+	  trailWidth: 1,
+	  easing: 'easeInOut',
+	  duration: 2200,
+	  text: {
+	    autoStyleContainer: false
+	  },
+	  from: { color: '#aaa', width: 1 },
+	  to: { color: '#ff7800', width: 4 },
+	  // Set default step function for all animate calls
+	  step: function(state, circle) {
+	    circle.path.setAttribute('stroke', state.color);
+	    circle.path.setAttribute('stroke-width', state.width);
+
+	    var value = Math.round(circle.value() * 100);
+	    if (value === 0) {
+	      circle.setText('');
+	    } else {
+	      circle.setText(value);
+	    }
+
+	  }
+	});
 }
