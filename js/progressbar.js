8a2aa117045fac94b8f6078c451924a52f043609
Melanie White <melwhite.a@gmail.com>
2016-07-15 14:10:41 -0400

    Fix progress bars :)

--- js/progressbar.js
+++ js/progressbar.js
@@ -1,13 +1,15 @@
 // progressbar.js@1.0.0 version is used
 // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
 
+var orange = "#ff7800";
+
 window.onload = function onLoad() {
 
 	var bar = new ProgressBar.Line('#openingline', {
 	  strokeWidth: 2,
 	  easing: 'easeInOut',
 	  duration: 1800,
-	  color: '#ff7800',
+	  color: orange,
 	  trailColor: '#eee',
 	  trailWidth: 1,
 	  svgStyle: {width: '100%', height: '100%'}
@@ -15,20 +17,20 @@ window.onload = function onLoad() {
 
 	bar.animate(1.0);  // Number from 0.0 to 1.0
 
-	var bar = new ProgressBar.Circle('#css', {
+	var bars = ['#css', '#jscript', '#ruby', '#uxd'].map(id => new ProgressBar.Circle(id, {
 
 	  color: '#aaa',
 	  // This has to be the same size as the maximum width to
 	  // prevent clipping
-	  strokeWidth: 4,
-	  trailWidth: 1,
+	  strokeWidth: 2,
+	  trailWidth: 10,
 	  easing: 'easeInOut',
-	  duration: 2200,
+	  duration: 1400,
 	  text: {
 	    autoStyleContainer: false
 	  },
-	  from: { color: '#aaa', width: 1 },
-	  to: { color: '#ff7800', width: 4 },
+	  from: { color: '#aaa', width: 2 },
+	  to: { color: orange, width: 10 },
 	  // Set default step function for all animate calls
 	  step: function(state, circle) {
 	    circle.path.setAttribute('stroke', state.color);
@@ -38,101 +40,16 @@ window.onload = function onLoad() {
 	    if (value === 0) {
 	      circle.setText('');
 	    } else {
-	      circle.setText(value);
+	      circle.setText(value+'%');
 	    }
-
 	  }
-	});
 
-	//bar.text.style.fontFamily = 'Helvetica, sans-serif';
-	//bar.text.style.fontSize = '2em';
+	}));
 
-	bar.animate(1.0);  // Number from 0.0 to 1.0
+	bars.forEach(bar => bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif');
+	bars.forEach(bar => bar.text.style.fontSize = '1.6rem');
+	bars.forEach(bar => bar.text.style.color = "#555" );
+
+	bars.forEach(bar => bar.animate(1));  // Number from 0.0 to 1.0
 	
-	var bar = new ProgressBar.Circle('#jscript', {
-
-	  color: '#aaa',
-	  // This has to be the same size as the maximum width to
-	  // prevent clipping
-	  strokeWidth: 4,
-	  trailWidth: 1,
-	  easing: 'easeInOut',
-	  duration: 2200,
-	  text: {
-	    autoStyleContainer: false
-	  },
-	  from: { color: '#aaa', width: 1 },
-	  to: { color: '#ff7800', width: 4 },
-	  // Set default step function for all animate calls
-	  step: function(state, circle) {
-	    circle.path.setAttribute('stroke', state.color);
-	    circle.path.setAttribute('stroke-width', state.width);
-
-	    var value = Math.round(circle.value() * 100);
-	    if (value === 0) {
-	      circle.setText('');
-	    } else {
-	      circle.setText(value);
-	    }
-
-	  }
-	});
-	
-	var bar = new ProgressBar.Circle('#ruby', {
-
-	  color: '#aaa',
-	  // This has to be the same size as the maximum width to
-	  // prevent clipping
-	  strokeWidth: 4,
-	  trailWidth: 1,
-	  easing: 'easeInOut',
-	  duration: 2200,
-	  text: {
-	    autoStyleContainer: false
-	  },
-	  from: { color: '#aaa', width: 1 },
-	  to: { color: '#ff7800', width: 4 },
-	  // Set default step function for all animate calls
-	  step: function(state, circle) {
-	    circle.path.setAttribute('stroke', state.color);
-	    circle.path.setAttribute('stroke-width', state.width);
-
-	    var value = Math.round(circle.value() * 100);
-	    if (value === 0) {
-	      circle.setText('');
-	    } else {
-	      circle.setText(value);
-	    }
-
-	  }
-	});
-	
-	var bar = new ProgressBar.Circle('#uxd', {
-
-	  color: '#aaa',
-	  // This has to be the same size as the maximum width to
-	  // prevent clipping
-	  strokeWidth: 4,
-	  trailWidth: 1,
-	  easing: 'easeInOut',
-	  duration: 2200,
-	  text: {
-	    autoStyleContainer: false
-	  },
-	  from: { color: '#aaa', width: 1 },
-	  to: { color: '#ff7800', width: 4 },
-	  // Set default step function for all animate calls
-	  step: function(state, circle) {
-	    circle.path.setAttribute('stroke', state.color);
-	    circle.path.setAttribute('stroke-width', state.width);
-
-	    var value = Math.round(circle.value() * 100);
-	    if (value === 0) {
-	      circle.setText('');
-	    } else {
-	      circle.setText(value);
-	    }
-
-	  }
-	});
 }
